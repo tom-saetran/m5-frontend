778d6b900e47fbeb1395421a18dc62de7210dc62
Tom-Lennart Sætran <79971807+tom-saetran@users.noreply.github.com>
2021-05-19 16:48:44 +0200

    init

--- src/views/blog/index.jsx
+++ src/views/blog/index.jsx
@@ -1,52 +1,56 @@
-import React, { Component } from "react";
-import { Container, Image } from "react-bootstrap";
-import { withRouter } from "react-router";
-import BlogAuthor from "../../components/blog/blog-author";
-import posts from "../../data/posts.json";
-import "./styles.css";
+import React, { Component } from "react"
+import { Container, Image } from "react-bootstrap"
+import { withRouter } from "react-router"
+import BlogAuthor from "../../components/blog/blog-author"
+import posts from "../../data/posts.json"
+import "./styles.css"
 class Blog extends Component {
-  state = {
-    blog: {},
-    loading: true,
-  };
-  componentDidMount() {
-    const { id } = this.props.match.params;
-    console.log(posts);
-    const blog = posts.find((post) => post._id.toString() === id);
-    if (blog) {
-      this.setState({ blog, loading: false });
-    } else {
-      this.props.history.push("/404");
+    state = {
+        blog: {},
+        loading: true,
+        endpoint: "http://localhost:8888/blogposts"
     }
-  }
 
-  render() {
-    const { loading, blog } = this.state;
-    if (loading) {
-      return <div>loading</div>;
-    } else {
-      return (
-        <div className="blog-details-root">
-          <Container>
-            <Image className="blog-details-cover" src={blog.cover} fluid />
-            <h1 className="blog-details-title">{blog.title}</h1>
-
-            <div className="blog-details-container">
-              <div className="blog-details-author">
-                <BlogAuthor {...blog.author} />
-              </div>
-              <div className="blog-details-info">
-                <div>{blog.createdAt}</div>
-                <div>{`${blog.readTime.value} ${blog.readTime.unit} read`}</div>
-              </div>
-            </div>
-
-            <div dangerouslySetInnerHTML={{ __html: blog.content }}></div>
-          </Container>
-        </div>
-      );
+    componentDidMount() {
+        const { id } = this.props.match.params
+        console.log(posts)
+        const blog = posts.find(post => post._id.toString() === id)
+        if (blog) {
+            this.setState({ blog, loading: false })
+        } else {
+            this.props.history.push("/404")
+        }
     }
-  }
+
+    get = () => {}
+
+    render() {
+        const { loading, blog } = this.state
+        if (loading) {
+            return <div>loading</div>
+        } else {
+            return (
+                <div className="blog-details-root">
+                    <Container>
+                        <Image className="blog-details-cover" src={blog.cover} fluid />
+                        <h1 className="blog-details-title">{blog.title}</h1>
+
+                        <div className="blog-details-container">
+                            <div className="blog-details-author">
+                                <BlogAuthor {...blog.author} />
+                            </div>
+                            <div className="blog-details-info">
+                                <div>{blog.createdAt}</div>
+                                <div>{`${blog.readTime.value} ${blog.readTime.unit} read`}</div>
+                            </div>
+                        </div>
+
+                        <div dangerouslySetInnerHTML={{ __html: blog.content }}></div>
+                    </Container>
+                </div>
+            )
+        }
+    }
 }
 
-export default withRouter(Blog);
+export default withRouter(Blog)
